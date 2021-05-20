422729a4439a9b9fd589b79555e4126442809ad7
marianomaccio <marianomaccio93@gmail.com>
2021-05-20 10:59:58 -0300

    se agrega inicio e imagenes

--- app/app.py
+++ app/app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/inicio')
+@app.route('/index')
 def index():
-    return render_template('inicio.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
