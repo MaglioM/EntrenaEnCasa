ea468972068b35545440a0ed7282992257f8d41e
magliom <matiashmaglio@gmail.com>
2021-09-18 13:42:55 -0300

    -Agregado conexión a la base para mostrar info de cursos -Agregado manejo de sesión

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -14,22 +14,23 @@ mysql = MySQL(app)
 #settings
 app.secret_key='secretkey'
 
+
+#Variables
+base='becridlgsxtglg0nm3o3'
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/boxeo')
-def boxeo():
-    return render_template('boxeo.html')
 
-@app.route('/kick-boxing')
-def kickboxing():
-    return render_template('kick-boxing.html')
-
-@app.route('/muay-thai')
-def muaythai():
-    return render_template('muay-thai.html')
+@app.route('/curso/<id>')
+def curso(id):    
+    session['idCurso']=id
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT Nombre FROM '+base+'.Curso WHERE IdCurso='+id)
+    curso = cursor.fetchall()[0][0]
+    return render_template('curso.html',curso=curso)
 
 
 @app.route('/register')
@@ -56,9 +57,9 @@ def registered():
                 password     = request.form['pass']
                 cursor       = mysql.connection.cursor()
                 if request.form['usuario'] == "Alumno":
-                    cursor.execute('INSERT INTO becridlgsxtglg0nm3o3.Alumnos (Nombre, Apellido, Contraseña, Email) VALUES (%s, %s, %s, %s)',(nombre, apellido, password, email))
+                    cursor.execute('INSERT INTO '+base+'.Alumnos (Nombre, Apellido, Contraseña, Email) VALUES (%s, %s, %s, %s)',(nombre, apellido, password, email))
                 elif request.form['usuario'] == "Instructor":
-                    cursor.execute('INSERT INTO becridlgsxtglg0nm3o3.Instructores (Nombre, Apellido, Contraseña, Email) VALUES (%s, %s, %s, %s)',(nombre, apellido, password, email))
+                    cursor.execute('INSERT INTO '+base+'.Instructores (Nombre, Apellido, Contraseña, Email) VALUES (%s, %s, %s, %s)',(nombre, apellido, password, email))
                 mysql.connection.commit()
                 return render_template('index.html')
             except:
@@ -72,26 +73,31 @@ def login():
 @app.route('/ingresado', methods=['POST'])
 def ingresado():
     if request.method == 'POST':
-        #saca campos de la base
+        #validación de login
         cursor = mysql.connection.cursor()
-        cursor.execute('SELECT Email FROM becridlgsxtglg0nm3o3.Alumnos')
+        cursor.execute('SELECT Email FROM '+base+'.Alumnos')
         emailsValidos = []
         for result in cursor.fetchall():
             for email in result:
                 emailsValidos.append(email)
-        cursor.execute('SELECT Contraseña FROM becridlgsxtglg0nm3o3.Alumnos')
+        cursor.execute('SELECT Contraseña FROM '+base+'.Alumnos')
         passValidas = []
         for result in cursor.fetchall():
             for password in result:
                 passValidas.append(password)
-        #validacion
         if request.form['email'] in emailsValidos:
-            cursor.execute('SELECT Contraseña FROM becridlgsxtglg0nm3o3.Alumnos WHERE Email = '+'"'+(request.form['email']+'"'))
+            cursor.execute('SELECT Contraseña FROM '+base+'.Alumnos WHERE Email = '+'"'+(request.form['email']+'"'))
             passValida = cursor.fetchall()[0][0]
             if request.form['pass'] == passValida:
-                cursor.execute('SELECT Nombre FROM becridlgsxtglg0nm3o3.Alumnos WHERE Email = '+'"'+(request.form['email']+'"'))
-                nombre = cursor.fetchall()[0][0]
-                return render_template('inicio.html',nombre=nombre)
+                #Buscar los cursos en la base para mostrar en pantalla
+                cursor.execute('SELECT * FROM '+base+'.Curso')
+                cursos = cursor.fetchall()
+                cursor.execute('SELECT Nombre, IdAlumno FROM '+base+'.Alumnos WHERE Email = '+'"'+(request.form['email']+'"'))
+                resultados=cursor.fetchall()
+                nombre = resultados[0][0]
+                #Guardar en la sesión al alumno y dirigirlo al inicio
+                session['idAlumno'] = resultados[0][1]
+                return render_template('inicio.html',nombre=nombre,cursos=cursos)
             else:
                 flash("Usuario no registrado")
                 return redirect(url_for('login'))
